145203925bbb8fb0db0d947cdee420ac365400a5
Pengcheng Tan <markqq@yeah.net>
2018-06-21 01:58:36 +0800

    Add files via upload

---
+++ example/example-1/default.js
@@ -0,0 +1,23 @@
+[!#JinStart name="standardCard"]
+<div style="background: white;
+            box-shadow: 0 1px 2px grey;
+            border-radius: 4px;
+            margin: 20px auto;
+            padding: 20px;
+            max-width: 300px;">
+	<h1 style="font-size: 18px;
+             color: #0e0e0e;
+             margin: 0;">
+    {{card_title}}
+  </h1>
+	<p style="color: #808080;
+            fonnt-size: 16px;
+            margin: 10px 0 0 0;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            overflow: ellipsis;">
+    {{card_content}}
+  </p>
+</div>
+[!#JinEnd]
